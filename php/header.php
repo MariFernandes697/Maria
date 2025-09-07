e68955f871732ffe05b242499d9dc882f655a1e5
Marianna Fernandes <mariannafernandes697@gmail.com>
2025-09-07 19:58:37 -0300

    página inicial

---
+++ php/header.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Maria</title>
+    <link rel="stylesheet" href="../front/index.css">
+</head>
+<body>
+    <header>
+   <div class="nav-bar">
+       <nav>
+            <ul>
+                <li><a href="#">Mariah</a></li>
+                <li><a href="about.php">Sobre</a></li>
+                <li><a href="loja.php">Loja</a></li>
+                <li><a href="ajuda.php">Ajuda</a></li>
+            </ul>
+       </nav>
+        </div>
+  </header>
+  <!-- Fim Head-->
+<?php 
+?>
---
+++ php/index.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+require_once('php/header.php');
+
+?>
+<main>
+<section class="main">
+    <div class="mundodev">
+        <img src="../assets/img/Programming-amico.svg" alt="">
+    </div>
+    <div class="mergulheaqui">
+        <span>
+            <h1>Mariah</h1>
+        </span>
+        <p>Bem Vindo ao Mariah Code!</p>
+        <p>Está pronto para mergulhar no universo da programação?</p>
+        <button><span class="text">Saiba mais</span></button>
+    </div>
+</section>
+</main>
+</body>
+</html>
